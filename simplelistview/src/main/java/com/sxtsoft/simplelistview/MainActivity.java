54de2f35260992d53552fbd6c6712360a17735f7
elespejodetarkovsky <sebastian.turone@gmail.com>
2019-06-14 12:08:33 +0200

    Se crea una nueva app de listview personalizada+

--- simplelistview/src/main/java/com/sxtsoft/simplelistview/MainActivity.java
+++ simplelistview/src/main/java/com/sxtsoft/simplelistview/MainActivity.java
@@ -2,20 +2,24 @@ package com.sxtsoft.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    ListView listView;
-    String[] valores = {"pepe","Carlos","Esteban","Tatiana","Federico","Juan",
+    private ListView listView;
+    private String[] valores = {"pepe","Carlos","Esteban","Tatiana","Federico","Juan",
             "Manuel","Sebastian","Adelaida","Alfonsia","Veronica","Yulei","Jin","Jordi"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listView = (ListView) findViewById(R.id.idListView);
 
         //necesitamos un adaptador
@@ -29,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 (this, android.R.layout.simple_expandable_list_item_1, valores);
 
         listView.setAdapter(adapter);
+
+        //TO DO, añadiremos un listener!
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String texto = "position: " + position + " Nombre: " + valores[position];
+                texto += " id: " + id + " view: " + view.getClass().getSimpleName();
+                texto += " parent: " +parent.getClass().getSimpleName();
+
+                Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
